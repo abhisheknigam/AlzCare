0a829c2953b5fbfeb419222158cad7be37fa04fe
Karan Vasnani <karanvasnani209@gmail.com>
2017-04-11 21:36:27 -0400

    pushing changes in node server

--- server/app.js
+++ server/app.js
@@ -13,8 +13,8 @@ var constant = require('./config/constants');*/
 var port = process.env.PORT || 8042;
 /*var mongoose = require('mongoose');
 var passport = require('passport');
-var flash = require('connect-flash');
-var path = require('path');*/
+var flash = require('connect-flash');*/
+var path = require('path');
 
 /*var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -39,20 +39,20 @@ var configDB = require('./config/database.js');
 mongoose.connect(configDB.url); // connect to our database
 
 
-/*require('./config/passport')(passport); // pass passport for configuration
+//require('./config/passport')(passport); // pass passport for configuration
 
 //set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
+//app.use(morgan('dev')); // log every request to the console
+//app.use(cookieParser()); // read cookies (needed for auth)
 //app.use(bodyParser()); // get information from html forms
 
 //view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
-//app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-
+/*
 //required for passport
 //app.use(session({ secret: 'iloveyoudear...' })); // session secret
 
@@ -76,10 +76,12 @@ console.log('The magic happens on port ' + port);
 
 //catch 404 and forward to error handler
 app.use(function (req, res, next) {
+	console.log(req.error);
     res.status(404).render('404', {title: "Sorry, page not found", session: req.sessionbo});
 });
 
 app.use(function (req, res, next) {
+	console.log(req.status);
     res.status(500).render('404', {title: "Sorry, page not found"});
 });
 exports = module.exports = app;
