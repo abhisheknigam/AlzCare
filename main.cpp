f5c2726bf9ea43cc6025144a0990b6078e6432f7
umanghans <umang.hans1994@gmail.com>
2017-04-11 22:12:47 -0400

    Add files via upload
    changed to mqtt

--- main.cpp
+++ main.cpp
@@ -1,151 +1,157 @@
-#include "string"
 #include "mbed.h"
+#include "MQTTClient.h"
+#include "MQTTEthernet.h"
+#include "rtos.h"
+#include "k64f.h"
 
-#include "EthernetInterface.h"
-#include "frdm_client.hpp"
+Serial PC(USBTX,USBRX);
 
+// connect options for MQTT broker
+//#define BROKER "broker.mqttdashboard.com"   // MQTT broker URL
+#define BROKER "192.168.0.14"   // MQTT broker URL
+#define PORT 1883                           // MQTT broker port number
+#define CLIENTID "fa41588a5ba7"    // use K64F MAC address without colons
+#define USERNAME ""                         // not required for MQTT Dashboard public broker 
+#define PASSWORD ""                         // not required for MQTT Dashboard public broker
+#define TOPIC1 "reset_sos"                            // MQTT topic
+#define TOPIC2 "sos"
 
+Queue<uint32_t, 6> messageQ;
 
-#define IOT_ENABLED
-
-//! The activation level of a circuit describes what voltage level is needed to
-//! make (in this case) an LED turn ON, or light up. Since the FRDM board's
-//! LEDs are active LOW, they must be pulled to GND (or 0, or false) to turn on.
-namespace active_low
-{
-	const bool on = false;
-	const bool off = true;
-}
-
-DigitalOut red_led(LED1);
-
-InterruptIn g_button_tap(SW2);
-
-//! Network requests cannot be created (getting/setting the resource objects) in
-//! secondary threads / interrupt contexts. Instead, we must mark that a value
-//! changed in these cases and have the main loop complete the operation.
-volatile bool sos_called = false;
-
-
-void flip() {
-    red_led = active_low::on;
-    wait(1);
-    red_led = active_low::off;
-}
-
-
-void on_tap()
-{
-    sos_called = true;
-        flip();
-}
-
-
-//! A utility function for formatting values to their string equivalent.
-void format_resource_value(size_t value, M2MResource* resource)
-{
-	//! Use sprintf to convert the integral value to a string.
-	char result_string[30];
-	size_t size = sprintf(result_string, "%u", value);
-	
-	const uint8_t* buffer = reinterpret_cast<const uint8_t*>(result_string);
-	resource->set_value(buffer, size);
-}
-
-int main()
-{
-	// Seed the RNG for networking purposes
-    unsigned seed = utils::entropy_seed();
-    srand(seed);
-
-	//! There was a mistake in the starting code that turned all LEDS on instead
-	//! of turning them off (active_low off/on constants were used in the
-	//! wrong place).
-    g_led_red = active_low::off;
-    g_led_green = active_low::off;
-    g_led_blue = active_low::off;
-
-	// Button falling edge is on push (rising is on release)
- 
-    g_button_tap.fall(&on_tap);
-
-#ifdef IOT_ENABLED
-	// Turn on the blue LED until connected to the network
-    g_led_blue = active_low::on;
-
-	// Need to be connected with Ethernet cable for success
-    EthernetInterface ethernet;
-    if (ethernet.connect() != 0)
-        return 1;
-
-	// Pair with the device connector
-    frdm_client client("coap://api.connector.mbed.com:5684", &ethernet);
-    if (client.get_state() == frdm_client::state::error)
-        return 1;
-
-	// The REST endpoints for this device
-	// Add your own M2MObjects to this list with push_back before client.connect()
-    M2MObjectList objects;
-
-    M2MDevice* device = frdm_client::make_device();
-    objects.push_back(device);
-    
-    //! ***********************
-    //! Begin Endpoint Creation
-    //! ***********************
-    
-    //! We first need to create the object and instance. Since we only have a
-    //! frequency counter (and one of them at that), we only need one object,
-    //! and one instance. The object describes the BPM counter interface in
-    //! general, while the instance represents a physical counter on the board.
-    M2MObject* SOS = M2MInterfaceFactory::create_object("3318");
-    M2MObjectInstance* sos_called = SOS->create_object_instance();
-    
-    //! Set Point allows the user to read/write the SOS value itself through
-    //! GET and PUT.
-    M2MResource* set_point = sos_called->create_dynamic_resource("5700", "integer", M2MResourceInstance::INTEGER, true);
-    set_point->set_operation(M2MBase::GET_PUT_ALLOWED);
-
-    
-    units->set_value(reinterpret_cast<const uint8_t*>("SOS"), 3);
-    
-    //! Once we create our needed endpoints, we have to push the OBJECT.
-    objects.push_back(SOS);
-    
-    //! *********************
-    //! End Endpoint Creation
-    //! *********************
-
-	// Publish the RESTful endpoints
-    client.connect(objects);
-
-	// Connect complete; turn off blue LED forever
-    g_led_blue = active_low::off;
-#endif
-
-    while (true)
-    {
-#ifdef IOT_ENABLED
-        if (client.get_state() == frdm_client::state::error)
+// LED color control function
+void controlLED(color_t led_color) {
+    switch(led_color) {
+        case red :
+            greenLED = blueLED = 1;          
+            redLED = 0.7;
             break;
-#endif
-
-        //! Here we must check for when our state is updated asynchronously,
-        //! and update the resource values as necessary.
-        if (sos_called)
-        {
-        	//! Our three values that can change may all be updated when the BPM
-        	//! changes, so just update them every time to be safe.
-        	format_resource_value(sos_called,set_point);
-        	
-        	sos_called = false;
-        }
-       
+        case green :
+            redLED = blueLED = 1;
+            greenLED = 0.7;
+            break;
+        case blue :
+            redLED = greenLED = 1;
+            blueLED = 0.7;
+            break;
+        case off :
+            redLED = greenLED = blueLED = 1;
+            break;
     }
+}
+    
+// Switch 2 interrupt handler
+void sw2_ISR(void) {
+    messageQ.put((uint32_t*)22);
+}
 
-#ifdef IOT_ENABLED
-    client.disconnect();
-#endif
+// Switch3 interrupt handler
+void sw3_ISR(void) {
+    messageQ.put((uint32_t*)33);
+}
+ 
+// MQTT message arrived callback function
+void messageArrived(MQTT::MessageData& md) {
+    MQTT::Message &message = md.message;
+    PC.printf("Receiving MQTT message:  %.*s\r\n", message.payloadlen, (char*)message.payload);
+    
+    if (message.payloadlen == 3) {
+        if (strncmp((char*)message.payload, "res", 3) == 0)
+            controlLED(green);
+    } 
+    
+}
 
-    return 1;
+int main() {
+    // turn off LED  
+    controlLED(off);
+    
+    // set SW2 and SW3 to generate interrupt on falling edge 
+    switch2.fall(&sw2_ISR);
+    z
+    
+    PC.printf("\r\n\r\nWelcome to the K64F MQTT Demo!\r\n");
+    PC.printf("\r\nAttempting connect to local network...\r\n");
+        
+    // initialize ethernet interface
+    MQTTEthernet ipstack = MQTTEthernet();
+    
+    // get and display client network info
+    EthernetInterface& eth = ipstack.getEth();
+    PC.printf("IP address is %s\r\n", eth.getIPAddress());
+    PC.printf("MAC address is %s\r\n", eth.getMACAddress());
+    PC.printf("Gateway address is %s\r\n", eth.getGateway());
+    
+    // construct the MQTT client
+    MQTT::Client<MQTTEthernet, Countdown> client = MQTT::Client<MQTTEthernet, Countdown>(ipstack);
+    
+    char* hostname = BROKER;
+    int port = PORT;
+    int rc;
+    
+    PC.printf("\r\nAttempting TCP connect to %s:%d:  ", hostname, port);
+    
+    // connect to TCP socket and check return code
+    if ((rc = ipstack.connect(hostname, port)) != 0)
+        PC.printf("failed: rc= %d\r\n", rc);
+        
+    else
+        PC.printf("success\r\n");
+    
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;       
+    data.MQTTVersion = 3;
+    data.clientID.cstring = CLIENTID;
+//    data.username.cstring = USERNAME;
+//    data.password.cstring = PASSWORD;
+    
+    // send MQTT connect packet and check return code
+    PC.printf("Attempting MQTT connect to %s:%d: ", hostname, port);
+    if ((rc = client.connect(data)) != 0)
+        PC.printf("failed: rc= %d\r\n", rc);
+        
+    else
+        PC.printf("success\r\n");
+        
+    char* reset_sos = TOPIC1;
+    char* sos = TOPIC2;
+    
+    // subscribe to MQTT topic1
+    PC.printf("Subscribing to MQTT topic %s: ", reset_sos);
+    if ((rc = client.subscribe(reset_sos, MQTT::QOS0, messageArrived)) != 0)
+        PC.printf("failed: rc= %d\r\n", rc);
+        
+    else
+        PC.printf("success\r\n");
+        
+    MQTT::Message message;
+    char buf[100];
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+    
+    while(true) {
+        osEvent switchEvent = messageQ.get(100);
+        
+        if (switchEvent.value.v == 22 || switchEvent.value.v == 33) {
+            //count++;
+            switch(switchEvent.value.v) {
+                case 22 :
+                    sprintf(buf, "sos");
+                    break;
+                case 33 :
+                    sprintf(buf, "sos");
+                    break;
+            }
+            //sprintf(buf, "1");
+            PC.printf("Publishing MQTT message: %.*s\r\n", message.payloadlen, (char*)message.payload);
+            
+            rc = client.publish(sos, message);
+            client.yield(100);
+        }
+        
+        else {
+            client.yield(100);
+        }           
+    }
 }
